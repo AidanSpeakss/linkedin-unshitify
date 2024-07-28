ca74de0762adcd4b409b453e29b34f71d3953652
Aidan <24593916+AidanSpeakss@users.noreply.github.com>
2024-07-27 21:01:20 -0500

    Update userscript.js
    made it better bro

--- userscript.js
+++ userscript.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         LinkedIn Unshitify
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/AidanSpeakss/linkedin-unshitify
+// @version      0.2
 // @description  try to take over the world!
-// @author       You
+// @author       You, Me, Bob, Your Uncle, Everyone Ideally
 // @run-at       document-start
 // @match        https://www.linkedin.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
@@ -15,17 +15,19 @@
     'use strict';
 function unshitify(){
 let newStyles = document.createElement('style');
-    //Removes Social Counter (Likes, Amount of Comments, Etc)
-    newStyles.innerText = ".social-details-social-counts__social-proof-container { display: none !important;}"
-    newStyles.innerText += ".social-details-social-counts { display: none !important}"
+    //Removes social counter from posts (likes, reactions, etc), this will also hide the comment section so you have to click comment if you want to make a comment or view comments
+    newStyles.innerText += ".social-details-social-counts__item, .social-details-social-counts__item, .social-details-reactors-facepile { display: none !important}"
 
-    //Removes Feed Entirely:
+    //Removes social counter from comments (likes, reactions, etc) while keep button to reply to comments
+    newStyles.innerText += ".comments-comment-social-bar__reactions-count--cr, .comments-comment-social-bar__replies-count--cr { display: none !important}"
+
+    //Removes Feed Entirely - Can Break Things:
     //newStyles.innerText += ".feed-new-update-pill__new-update-button, .feed-right-rail, .feed-shared-update-v2, .nav-item__badge--doughnut, .feed-shared-news-module, .feed-follows-module { display: none !important; }"
 
-    //Less Fucked Removed Feed Entirely:
+    //Removed Feed Entirely - Does Not Break As Many Things:
     //newStyles.innerText += ".feed-shared-update-v2 { display: none !important; }"
 
-    //Removes profile view stats on sidebar
+    //Removes profile view stats
     newStyles.innerText += ".feed-identity-module__widgets { display: none !important}"
 
     //Removes sidebar with news and games
@@ -34,8 +36,11 @@ let newStyles = document.createElement('style');
     //Removes ai comment suggestions
     newStyles.innerText += ".feed-shared-coach-prompt__multi-suggestion-cta { display: none !important}"
 
-    //Removes liked and commented on posts from feed (only showing POSTS from people you follow) - cant get to work write, feel free to improve with a fork and merge it
+    //Removes liked and commented on posts from feed (only showing POSTS from people you follow) - cant get to work right, feel free to improve with a fork and merge it
     //newStyles.innerText += ".feed-shared-update-v2:has(> div.update-components-header)  { display: none !important}"
+
+    //Removes follow button
+    newStyles.innerText += ".follow { display: none !important}"
 
 document.head.appendChild(newStyles);
 }
